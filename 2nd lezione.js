dde3f6916f36898eba4b9029784e9514b05ae13d
joyzes <joyizes@yahoo.com>
2025-05-08 18:17:23 +0200

    preparazione x 3rd lezione

---
+++ 2nd lezione.js
@@ -0,0 +1,30 @@
+const lunghezza = 200;
+
+function latodellacasa() {
+    forward(lunghezza);
+    right(90);
+}
+
+function tornaSu() {
+right(45)
+forward(lunghezza *1.414);
+}
+
+function tetto () }
+
+    latodellacasa();
+    latodellacasa();
+    latodellacasa();
+    latodellacasa();
+
+
+    function tornaSu() {
+        right(45)
+// right(90);
+// forward(lunghezza);
+// right(90);
+// forward(lunghezza);
+// right(90);
+// forward(lunghezza);
+// right(90+45);
+// forward(lunghezza *1.414/2);
